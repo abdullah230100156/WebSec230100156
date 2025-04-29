82ae5e25f43774e275e3aef01d115f4322a3ad89
abdullah230100156 <abdullah230100156@sut.edu.eg>
2025-04-29 23:21:47 +0300

    orgnize the web file

--- WebSecService/routes/web.php
+++ WebSecService/routes/web.php
@@ -1,4 +1,5 @@
 <?php
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
@@ -6,7 +7,6 @@ use App\Http\Controllers\Web\ProductsController;
 use App\Http\Controllers\Web\UsersController;
 use App\Http\Controllers\Web\PurchaseController;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
-
 
 Route::get('register', [UsersController::class, 'register'])->name('register');
 Route::post('register', [UsersController::class, 'doRegister'])->name('do_register');
@@ -21,8 +21,6 @@ Route::get('users/delete/{user}', [UsersController::class, 'delete'])->name('use
 Route::get('users/edit_password/{user?}', [UsersController::class, 'editPassword'])->name('edit_password');
 Route::post('users/save_password/{user}', [UsersController::class, 'savePassword'])->name('save_password');
 
-
-
 Route::get('products', [ProductsController::class, 'list'])->name('products_list');
 Route::get('products/edit/{product?}', [ProductsController::class, 'edit'])->name('products_edit');
 Route::post('products/save/{product?}', [ProductsController::class, 'save'])->name('products_save');
@@ -33,7 +31,7 @@ Route::get('/', function () {
 });
 
 Route::get('/multable', function (Request $request) {
-    $j = $request->number??5;
+    $j = $request->number ?? 5;
     $msg = $request->msg;
     return view('multable', compact("j", "msg"));
 });
@@ -49,14 +47,13 @@ Route::get('/prime', function () {
 Route::get('/test', function () {
     return view('test');
 });
+
 Route::post('/purchase/{id}', [ProductsController::class, 'purchase'])->name('product.purchase');
 Route::get('/my-purchases', [PurchaseController::class, 'myPurchases'])->name('my.purchases');
-
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/profile', [UsersController::class, 'profile'])->name('profile');
 });
-
 
 Route::middleware('auth')->group(function () {
     Route::get('/purchases', [PurchaseController::class, 'myPurchases'])->name('my.purchases');
@@ -65,21 +62,19 @@ Route::middleware('auth')->group(function () {
     Route::delete('/purchase/{id}', [PurchaseController::class, 'destroy'])->name('purchase.destroy');
 });
 
-
 Route::get('/email/verify', function () {
     return view('auth.verify-email');
 })->middleware('auth')->name('verification.notice');
 
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill();
-    return redirect('/'); // أو أي صفحة بعد التفعيل
+    return redirect('/');
 })->middleware(['auth', 'signed'])->name('verification.verify');
 
 Route::post('/email/verification-notification', function (Request $request) {
     $request->user()->sendEmailVerificationNotification();
     return back()->with('message', 'Verification link sent!');
 })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
-
 
 Route::post('/product/{id}/purchase', [PurchaseController::class, 'purchaseProduct'])->name('product.purchase');
 
@@ -89,34 +84,23 @@ Route::post('/users/store', [UsersController::class, 'store'])->name('users_stor
 Route::get('/users/add-credit', [\App\Http\Controllers\Web\UsersController::class, 'addCreditForm'])->name('credit.form');
 Route::post('/users/add-credit', [\App\Http\Controllers\Web\UsersController::class, 'storeCredit'])->name('credit.store');
 
-
 Route::delete('/users/{id}', [UsersController::class, 'destroy'])->name('users_delete');
 
 Route::post('/reset-credit', [UsersController::class, 'resetCredit'])->name('reset.credit');
 
 Route::get('/purchases', [PurchaseController::class, 'myPurchases'])->name('purchases.index');
 
-
-
-
-
 Route::get('/verify', [UsersController::class, 'verify'])->name('verify');
 // ############################
 Route::get('verify', [UsersController::class, 'verify'])->name('verify');
-// ############################ 
-
+// ############################
 
 Route::get('auth/facebook', [UsersController::class, 'redirectToFacebook'])->name('redirectToFacebook');
-
 Route::get('auth/facebook/callback', [UsersController::class, 'handleFacebookCallback'])->name('handleFacebookCallback');
-
-
-
-
 
 // xss injection
 Route::get('sqli', function (Request $request) {
-    $table=$request->query('table');
+    $table = $request->query('table');
     DB::unprepared(("DROP TABLE $table"));
     return redirect('/');
 });
@@ -131,7 +115,3 @@ Route::get('/collect', function (Request $request) {
         ->header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
         ->header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With');
 });
-
-
-
-
